c012a9b6dfeea551b5ef41bedf61b3e48aafec4e
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-01-06 04:22:35 +0100

    Changed windows size

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "mkvsplitter",
+		Title:  "MKV Splitter",
 		Width:  1024,
-		Height: 768,
+		Height: 268,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
